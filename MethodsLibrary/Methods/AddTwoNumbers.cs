c61581dffb287c4c25c5007cfc091481a29a788e
xiaotong.xu <xiaotong.xu@TNHQ-XXU>
2017-03-22 17:33:06 -0400

    Implement addTwoNumbers and add constructor for ListNode

--- MethodsLibrary/Methods/AddTwoNumbers.cs
+++ MethodsLibrary/Methods/AddTwoNumbers.cs
@@ -4,10 +4,54 @@ namespace MethodsLibrary.Methods
 {
     public class AddTwoNumbers
     {
+        /// <summary>
+        /// You are given two non-empty linked lists representing two non-negative integers. 
+        /// The digits are stored in reverse order and each of their nodes contain a single digit. 
+        /// Add the two numbers and return it as a linked list.
+        /// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        /// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        /// Output: 7 -> 0 -> 8
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
         public ListNode addTwoNumbers(ListNode l1, ListNode l2)
         {
-            return null;
-        }
+            if (l1 == null)
+            {
+                return l2;
+            }
+            if (l2 == null)
+            {
+                return l1;
+            }
 
+            ListNode calNode = new ListNode();
+            ListNode resultNode = calNode;
+            int jinWei = 0;
+
+            while ((l1 != null) && (l2 != null))
+            {
+                calNode.Val = l1.Val + l2.Val + jinWei;
+                jinWei = jinWei % 10;
+                calNode.Next = new ListNode();
+                calNode = calNode.Next;
+                l1 = l1.Next;
+                l2 = l2.Next;
+            }
+
+            if (l1 == null)
+            {
+                calNode.Val = jinWei + l2.Val;
+                return resultNode.Next;
+            }
+
+            if (l2 == null)
+            {
+                calNode.Val = jinWei + l1.Val;
+                return resultNode.Next;
+            }
+            return resultNode.Next;
+        }
     }
 }
--- MethodsLibrary/Objects/ListNode.cs
+++ MethodsLibrary/Objects/ListNode.cs
@@ -5,5 +5,10 @@
         public int Val;
         public ListNode Next;
         public ListNode(int x) { Val = x; }
+
+        public ListNode()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
