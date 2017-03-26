671a25d2b3a1646735588eb3a1888ba96b9a18ef
xxxrrraaayyy <tongbaxu@gmail.com>
2017-03-26 19:34:02 -0400

    New method and test data class for LengthOfLongestSubstring

---
+++ MethodsLibrary/Methods/LengthOfLongestSubstring.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsLibrary.Methods
+{
+    public class LengthOfLongestSubstring
+    {
+        public int lengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+
+
+
+            return result;
+        }
+    }
+}
