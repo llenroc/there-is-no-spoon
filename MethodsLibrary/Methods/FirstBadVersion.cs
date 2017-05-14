fb0903ee6a2ea3715fbf304a7f3e60fabe6c6785
xxxrrraaayyy <tongbaxu@gmail.com>
2017-05-14 19:31:07 -0400

    Add FirstBadVersion Method. Test is to implemented.

--- MethodsLibrary/Methods/FirstBadVersion.cs
+++ MethodsLibrary/Methods/FirstBadVersion.cs
@@ -1,16 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MethodsLibrary.Objects;
+
 
 namespace MethodsLibrary.Methods
 {
-    class FirstBadVersion
+    public class FirstBadVersion : VersionControl
     {
-        public int firstBadVersion(int n)
+        public static int firstBadVersion(int n)
         {
-            return 0;
+            if (n==0 || n==1)
+            {
+                return n;
+            }
+
+            int start = 1; 
+            int version = 0;
+            while (start <= n)
+            {
+                version = (start + n) / 2;
+                if(IsBadVersion(version))
+                {
+                    n = version - 1;
+                }
+                else
+                {
+                    start = version + 1;
+                }
+            }
+            return n;
         }
     }
+
+
+
 }
---
+++ MethodsLibrary/Objects/VersionControl.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MethodsLibrary.Objects
+{
+    public class VersionControl
+    {
+        public static int BadVersion { set; get; }
+
+        public static bool IsBadVersion(int version)
+        {
+            return version == BadVersion;
+        }
+    }
+}
