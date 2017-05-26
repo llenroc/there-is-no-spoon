73099f1b49d0af51c90d4f6f4fe1809316d928bc
xxxrrraaayyy <tongbaxu@gmail.com>
2017-05-25 23:44:41 -0400

    MergeTwoLists to be implemented

---
+++ MethodsLibrary/Methods/MergeTwoSortedLists.cs
@@ -0,0 +1,14 @@
+﻿
+using MethodsLibrary.Objects;
+
+namespace MethodsLibrary.Methods
+{
+    public class MergeTwoSortedLists
+    {
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+
+            return null;
+        }
+    }
+}
