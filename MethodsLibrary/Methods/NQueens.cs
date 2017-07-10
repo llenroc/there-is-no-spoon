1d4e15bbfa96ef113fa63b7351e26575a67bbe83
xxxrrraaayyy <tongbaxu@gmail.com>
2017-07-09 23:55:15 -0400

    NQueens to be implemented

---
+++ MethodsLibrary/Methods/NQueens.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MethodsLibrary.Methods
+{
+    public class NQueens
+    {
+        public static IList<IList<string>> SolveNQueens(int n)
+        {
+            
+        }
+
+        public static bool isLegit()
+        {
+
+        }
+    }
+}
