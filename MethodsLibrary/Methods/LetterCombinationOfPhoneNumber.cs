f9dae37d05c0a0bb0c26134a6d855af3651da1ed
xxxrrraaayyy <tongbaxu@gmail.com>
2017-08-10 02:53:42 -0400

    Method for LetterCombinationOfPhone - to be implemented

--- MethodsLibrary/Methods/LetterCombinationOfPhoneNumber.cs
+++ MethodsLibrary/Methods/LetterCombinationOfPhoneNumber.cs
@@ -12,8 +12,32 @@ namespace MethodsLibrary.Methods
         //Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
         public static IList<string> LetterCombinations(string digits)
         {
+            IList<string> result = new List<string>();
 
-            return null;
+            if(digits == null || digits.Length == 0)
+            {
+                return result;
+            }
+
+            Dictionary<char, string> panel = new Dictionary<char, string>
+            {
+                { '1', "" },
+                { '2', "abc" },
+                { '3', "def" },
+                { '4', "ghi" },
+                { '5', "jkl" },
+                { '6', "mno" },
+                { '7', "pqr" },
+                { '8', "tuv" },
+                { '9', "wxyz" },
+                { '0', " " },
+                { '*', "" },
+                { '#', "" }
+            };
+
+            
+
+            return result;
         }
 
         
