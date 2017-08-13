b822af8ce49b5acdb663581afdd70acd95f91128
xxxrrraaayyy <tongbaxu@gmail.com>
2017-08-13 03:29:42 -0400

    Method and test for Longest Consecutive Sequence - Mine works, but not the best :(

---
+++ Methods.Tests/Tests/LongestConsecutiveSequenceTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Methods.Tests.Objects;
+using MethodsLibrary.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Methods.Tests.Tests
+{
+    [TestClass]
+    public class LongestConsecutiveSequenceTest
+    {
+        /// <summary>
+        /// List to store test data set
+        /// </summary>
+        public List<LongestConsecutiveSequenceTestData> TestDataList { set; get; }
+
+        /// <summary>
+        /// Initialize test data for TwoSum
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            TestDataList = new List<LongestConsecutiveSequenceTestData>
+            {
+                new LongestConsecutiveSequenceTestData
+                {
+                    InputArray = new[] {100, 4, 200, 1, 3, 2},
+                    OutputInt = 4
+                },
+                new LongestConsecutiveSequenceTestData
+                {
+                    InputArray = new[] {7, 4, 8, 1, 3, 2, 9, 10, 11 },
+                    OutputInt = 5
+                },
+                new LongestConsecutiveSequenceTestData
+                {
+                    InputArray = new[] {100},
+                    OutputInt = 1
+                },
+                new LongestConsecutiveSequenceTestData
+                {
+                    InputArray = new[] { 1, 2, 0, 1},
+                    OutputInt = 3
+                },
+                new LongestConsecutiveSequenceTestData
+                {
+                    InputArray = new[] { 0, 0 },
+                    OutputInt = 1
+                }
+            };
+        }
+
+        /// <summary>
+        /// Test method to verify if the method works
+        /// </summary>
+        [TestMethod]
+        public void longestConsecutiveSequenceTest()
+        {
+            foreach (LongestConsecutiveSequenceTestData testData in TestDataList)
+            {
+                Console.WriteLine("Test iutput: " + string.Join(",", testData.InputArray));
+                Console.WriteLine("Expected Output: " + testData.OutputInt);
+                int result = LongestConsecutiveSequence.LongestConsecutive(testData.InputArray);
+                Console.WriteLine("Actual Output: " + result);
+                Assert.AreEqual(testData.OutputInt, result);
+            }
+        }
+
+    }
+}
--- MethodsLibrary/Methods/LongestConsecutiveSequence.cs
+++ MethodsLibrary/Methods/LongestConsecutiveSequence.cs
@@ -6,11 +6,110 @@ using System.Threading.Tasks;
 
 namespace MethodsLibrary.Methods
 {
-    class LongestConsecutiveSequence
+    public class LongestConsecutiveSequence
     {
-        public int LongestConsecutive(int[] nums)
+        /*
+         * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+         * For example, Given [100, 4, 200, 1, 3, 2], 
+         * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.  
+         */
+        public static int LongestConsecutive(int[] nums)
         {
-            return 0;
+            if (nums.Length == 0 || nums == null) return 0;
+            if (nums.Length == 1) return 1;
+            int maxLength = 1;
+            int length = 1;
+            Array.Sort(nums);
+
+            int i = 1;
+            while (i < nums.Length)
+            {
+                if(nums[i] == nums[i - 1])
+                {
+                    i++;
+                    continue;
+                }
+                if(nums[i]-1 == nums[i-1])
+                {
+                    length++;
+                    maxLength = Math.Max(length, maxLength);
+                }
+                else
+                {
+                    maxLength = Math.Max(length, maxLength);
+                    length = 1;
+                }
+                i++;
+            }
+            return maxLength;
+        }
+
+        /// <summary>
+        /// Best performance but I didn't write this :)
+        /// Paste here for me to learn
+        /// </summary>
+        /// <param name="nums"> input int array </param>
+        /// <returns> the length of the longest consecutive elements sequence </returns>
+        public static int LongestConsecutiveDictionary(int[] nums)
+        {
+            IDictionary<int, int> map = new Dictionary<int, int>();
+            int maxSeqLength = 0;
+
+            foreach (int num in nums)
+            {
+                if (!map.ContainsKey(num))
+                {
+                    int left = 0, right = 0;
+                    map.TryGetValue(num - 1, out left);
+                    map.TryGetValue(num + 1, out right);
+
+                    int sum = left + right + 1;
+                    maxSeqLength = Math.Max(maxSeqLength, sum);
+                    map.Add(num, sum);
+                    map[num - left] = sum;
+                    map[num + right] = sum;
+                }
+            }
+
+            return maxSeqLength;
+        }
+
+        /// <summary>
+        /// Better performance but I didn't write this :)
+        /// Paste here for me to learn
+        /// </summary>
+        /// <param name="nums"> input int array </param>
+        /// <returns> the length of the longest consecutive elements sequence </returns>
+        public static int LongestConsecutiveHashSet(int[] nums)
+        {
+            HashSet<int> hashSet = new HashSet<int>(nums);
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashSet.Contains(nums[i]))
+                {
+                    int count = 1;
+                    int next = nums[i] - 1;
+                    hashSet.Remove(nums[i]);
+                    while (hashSet.Contains(next))
+                    {
+                        hashSet.Remove(next);
+                        next--;
+                        count++;
+                    }
+                    next = nums[i] + 1;
+                    while (hashSet.Contains(next))
+                    {
+                        hashSet.Remove(next);
+                        next++;
+                        count++;
+                    }
+                    max = Math.Max(max, count);
+                }
+
+
+            }
+            return max;
         }
     }
 }
