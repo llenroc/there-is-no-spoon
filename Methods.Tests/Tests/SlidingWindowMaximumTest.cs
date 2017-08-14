52bfed2accea30e2e2ded1d7dfa722e5f0fd0e72
xxxrrraaayyy <tongbaxu@gmail.com>
2017-08-14 01:53:10 -0400

    Methods and test for Sliding Window Maximum - Mine is the basic one so it's not the best :(

---
+++ Methods.Tests/Tests/SlidingWindowMaximumTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Methods.Tests.Objects;
+using MethodsLibrary.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Methods.Tests.Tests
+{
+    [TestClass]
+    public class SlidingWindowMaximumTest
+    {
+        /// <summary>
+        /// List to store test data set
+        /// </summary>
+        public List<SlidingWindowMaximumTestData> TestDataList { set; get; }
+
+        /// <summary>
+        /// Initialize test data for TwoSum
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            TestDataList = new List<SlidingWindowMaximumTestData>
+            {
+                new SlidingWindowMaximumTestData
+                {
+                    InputNumber = 3,
+                    InputArray = new[] { 1, 3, -1, -3, 5, 3, 6, 7},
+                    OutputArray = new[] { 3, 3, 5, 5, 6, 7 }
+                },
+                new SlidingWindowMaximumTestData
+                {
+                    InputNumber = 0,
+                    InputArray = new int[] { },
+                    OutputArray = new int[] { }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Test method to verify if the method works
+        /// </summary>
+        [TestMethod]
+        public void slidingWindowMaximumTest()
+        {
+            foreach (SlidingWindowMaximumTestData testData in TestDataList)
+            {
+                Console.WriteLine("Test input k = " + testData.InputNumber);
+                Console.WriteLine("Test input array: " + string.Join(",", testData.InputArray));
+                Console.WriteLine("Expected output: " + string.Join(",", testData.OutputArray));
+                int[] result = SlidingWindowMaximum.MaxSlidingWindow(testData.InputArray, testData.InputNumber);
+                Console.WriteLine("Actual output: " + string.Join(",", result));
+                CollectionAssert.AreEqual(testData.OutputArray, result);
+            }
+        }
+
+    }
+}
---
+++ MethodsLibrary/Methods/SlidingWindowMaximum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodsLibrary.Methods
+{
+    public class SlidingWindowMaximum
+    {
+        //[1,3,-1,-3,5,3,6,7] 3
+        public static int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            int[] result = new int[nums.Length - k + 1];
+
+            if (nums.Length == 0 || nums == null || k<=0)
+            {
+                return nums; //new int[] { };
+            }
+
+            for (int i = 0; i <= nums.Length - k; i++)
+            {
+                int max = int.MinValue;
+
+                int n = 0;
+                while (n < k)
+                {
+                    max = Math.Max(max, nums[i+n]);
+                    n++;
+                }
+                result[i] = max;
+            }
+            return result;
+        }
+
+        // Better performance, but not mine :(
+        public int[] MaxSlidingWindowRevised(int[] nums, int k)
+        {
+            List<int> res = new List<int>();
+            var list = new LinkedList<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (list.Count > 0 && list.First.Value == i - k)
+                {
+                    list.RemoveFirst();
+                }
+                while (list.Count > 0 && nums[list.Last.Value] < nums[i])
+                {
+                    list.RemoveLast();
+                }
+                list.AddLast(i);
+                if (i >= k - 1)
+                {
+                    res.Add(nums[list.First.Value]);
+                }
+            }
+            return res.ToArray();
+        }
+    }
+}
