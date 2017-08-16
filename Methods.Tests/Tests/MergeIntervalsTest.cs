d4b38adb2b3895218fe52d136cce4416e647fddc
xxxrrraaayyy <tongbaxu@gmail.com>
2017-08-16 02:08:45 -0400

    Method for Merge Intervals - to be continued

---
+++ Methods.Tests/Tests/MergeIntervalsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Methods.Tests.Objects;
+using MethodsLibrary.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MethodsLibrary.Objects;
+
+namespace Methods.Tests.Tests
+{
+    [TestClass]
+    public class MergeIntervalsTest
+    {
+        private IList<Interval> inputIntervals;
+        private IList<Interval> outputIntervals;
+
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            inputIntervals = new List<Interval>
+            {
+                new Interval(1,3),
+                new Interval(2,6),
+                new Interval(8,10),
+                new Interval(15,18)
+            };
+            outputIntervals = new List<Interval>
+            {
+                new Interval(1,6),
+                new Interval(8,10),
+                new Interval(15,18)
+            };
+
+        }
+
+        [TestMethod]
+        public void mergeIntervalsTest()
+        {
+            IList<Interval> result = MergeIntervals.Merge(inputIntervals);
+
+            foreach(Interval interval in result)
+            {
+                Console.WriteLine("Start: " + interval.start + ", " + "End: " + interval.end);
+            }
+
+            foreach (Interval interval in outputIntervals)
+            {
+                Console.WriteLine("Start: " + interval.start + ", " + "End: " + interval.end);
+            }
+
+        }
+    }
+}
---
+++ MethodsLibrary/Methods/MergeIntervals.cs
@@ -0,0 +1,24 @@
+﻿
+using MethodsLibrary.Objects;
+using System.Collections.Generic;
+
+namespace MethodsLibrary.Methods
+{
+    public class MergeIntervals
+    {
+        public static IList<Interval> Merge(IList<Interval> intervals)
+        {
+            IList<Interval> result = new List<Interval>();
+            if(intervals.Count == 0 || intervals == null)
+            {
+                return result;
+            }
+
+
+
+
+
+            return result;
+        }
+    }
+}
