4c63c30451558f06200e459c906a65d5041dda81
xxxrrraaayyy <tongbaxu@gmail.com>
2017-08-31 03:19:46 -0400

    Method and test for Sudoku Solver - goSolver works. Trying to see why goSolver2 doesn't

--- Methods.Tests/Tests/SudokuSolverTest.cs
+++ Methods.Tests/Tests/SudokuSolverTest.cs
@@ -37,6 +37,23 @@ namespace Methods.Tests.Tests
                         {'.', '.', '.', '.', '8', '.', '.', '7', '9' }
                     },
                     OutputBool = true
+                },
+                new ValidSudokuTestData
+                {
+                    // Invalid game
+                    InputArray = new[,]
+                    {
+                        {'5', '3', '1', '.', '7', '.', '.', '.', '.' },
+                        {'6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                        {'.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                        {'8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                        {'4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                        {'7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                        {'.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                        {'.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                        {'.', '.', '.', '.', '8', '.', '.', '7', '9' }
+                    },
+                    OutputBool = true
                 }
             };
         }
@@ -60,7 +77,7 @@ namespace Methods.Tests.Tests
                     Console.WriteLine();
                 }
                 SudokuSolver.SolveSudoku(matrix);
-                Console.WriteLine("Actual output: " );
+                Console.WriteLine("Actual output: ");
                 for (int i = 0; i <= matrix.GetUpperBound(0); i++)
                 {
                     for (int j = 0; j <= matrix.GetUpperBound(1); j++)
--- MethodsLibrary/Methods/SudokuSolver.cs
+++ MethodsLibrary/Methods/SudokuSolver.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace MethodsLibrary.Methods
 {
@@ -7,48 +6,84 @@ namespace MethodsLibrary.Methods
     {
         public static void SolveSudoku(char[,] board)
         {
-            if (!isValidSudoku(board))
-            {
-                return;
-            }
             if (board == null || board.Length == 0)
             {
                 return;
             }
-
-            goSolver(board, 0, 0);
-
-        }
-
-        public static void goSolver(char[,] board, int rows, int columns)
-        {
             if (!isValidSudoku(board))
             {
                 return;
             }
+            goSolver2(board);
+        }
 
-            if (isValidSudoku(board) && rows == 8 && columns == 8)
+        public static bool goSolver(char[,] board)
+        {
+            if (!isValidSudoku(board))
             {
-                return;
+                return false;
             }
-
             for (int row = 0; row < 9; row++)
             {
                 for (int column = 0; column < 9; column++)
                 {
                     if (board[row, column] == '.')
                     {
-                        for (int n = 1; n <= 9; n++)
+                        for (char n = '1'; n <= '9'; n++)
                         {
-                            board[row, column] = (char)n;
-                            goSolver(board, row, column);
+                            //board[row, column] = Convert.ToChar(n + 48); Use this if the n is int
+                            board[row, column] = n;
+                            if (goSolver(board))
+                            {
+                                return true;
+                            }
+                            // The returned false comes from isValidSudoku(board)
+                            // it means it's not a valid solution
+                            // so set it back to '.' for the next try
+                            else
+                            {
+                                board[row, column] = '.';
+                            }
+                        }
+                        //When every number doesn't make it valid, it's a wrong path.
+                        //Return false to get to the last calling and set it back to '.'
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static void goSolver2(char[,] board)
+        {
+            if (!isValidSudoku(board))
+            {
+                return;
+            }
+            for (int row = 0; row < 9; row++)
+            {
+                for (int column = 0; column < 9; column++)
+                {
+                    if (board[row, column] == '.')
+                    {
+                        for (char n = '1'; n <= '9'; n++)
+                        {
+                            //board[row, column] = Convert.ToChar(n + 48); Use this if the n is int
+                            board[row, column] = n;
+                            goSolver2(board);                
                         }
                         board[row, column] = '.';
+                        return;
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Method to check if the sudoku board is valid
+        /// </summary>
+        /// <param name="board">2D array stands for the sudoku game board</param>
+        /// <returns>True if the sudoku board is valid</returns>
         public static bool isValidSudoku(char[,] board)
         {
             if (board == null || board.Length == 0)
@@ -65,7 +100,7 @@ namespace MethodsLibrary.Methods
                 {
                     if (set.Contains(board[i, j]))
                     {
-                        System.Console.WriteLine("Is it here? row");
+                        //System.Console.WriteLine("Is it here? row");
                         return false;
                     }
                     if (board[i, j] != '.')
@@ -83,7 +118,7 @@ namespace MethodsLibrary.Methods
                 {
                     if (set.Contains(board[j, i]))
                     {
-                        System.Console.WriteLine("Is it here? column");
+                        //System.Console.WriteLine("Is it here? column");
                         return false;
                     }
                     if (board[j, i] != '.')
@@ -106,7 +141,7 @@ namespace MethodsLibrary.Methods
                     {
                         if (set.Contains(board[i + row * 3, j + column * 3]))
                         {
-                            System.Console.WriteLine("Is it here?");
+                            //System.Console.WriteLine("Is it here?");
                             return false;
                         }
                         if (board[i + row * 3, j + column * 3] != '.')
