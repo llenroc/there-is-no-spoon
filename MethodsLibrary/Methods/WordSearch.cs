5118bb32ce9c92c2b67e136df9921748cd412071
xxxrrraaayyy <tongbaxu@gmail.com>
2017-10-01 02:04:02 -0400

    Method and test for Word Search - Be careful with what you pasted!

--- MethodsLibrary/Methods/WordSearch.cs
+++ MethodsLibrary/Methods/WordSearch.cs
@@ -5,36 +5,41 @@ namespace MethodsLibrary.Methods
     {
         public static bool Exist(char[,] board, string word)
         {
-
-
-
-
-            return isExist(0, 0, 0, board, word);
-        }
-
-        public static bool isExist(int row, int column, int index, char[,] board, string word)
-        {
-            if (index == word.Length - 1)
-            {
-                return true;
-            }
-            int rows = board.GetUpperBound(0);
-            int columns = board.GetUpperBound(1);
-            if (row < 0 || row >= rows || column < 0 || column >= columns)
-            {
+            if (board == null || board.GetLength(0) == 0 || board.GetLength(1) == 0)
                 return false;
-            }
-            for (int i = 0; i < word.Length; i++)
+            if (string.IsNullOrEmpty(word))
+                return true;
+
+            // OMG, I had a hard time troubleshooting why the test failed.
+            // It tured out I used board.GetUpperBound() instead of board.GetLength()
+            // Be careful with that next time 
+            int rows = board.GetLength(0);
+            int columns = board.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
             {
-                if (word[i] == board[row, column])
+                for (int j = 0; j < columns; j++)
                 {
-                    isExist(row - 1, column, i, board, word);
-                    isExist(row + 1, column, i, board, word);
-                    isExist(row, column - 1, i, board, word);
-                    isExist(row, column + 1, i, board, word);
+                    if (isExist(board, i, j, word, 0)) return true;
                 }
             }
             return false;
+        }
+
+        public static bool isExist(char[,] board, int row, int column, string word, int index)
+        {
+            if (index == word.Length) return true;
+            if (row < 0 || row >= board.GetLength(0) || column < 0 || column >= board.GetLength(1) || word[index] != board[row, column]) return false;
+            
+            char temp = board[row, column];
+            board[row, column] = '#';
+            bool exist = isExist(board, row, column + 1, word, index + 1) ||
+                         isExist(board, row, column - 1, word, index + 1) ||
+                         isExist(board, row + 1, column, word, index + 1) ||
+                         isExist(board, row - 1, column, word, index + 1);
+            board[row, column] = temp;
+
+            return exist;
         }
     }
 }
