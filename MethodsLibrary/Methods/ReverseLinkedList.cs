3984c82cba8403120289ecc17b005427d4f55e22
xxxrrraaayyy <tongbaxu@gmail.com>
2017-10-10 01:58:48 -0400

    Method and test for Reverse Linked List - not too bad huh :)

---
+++ MethodsLibrary/Methods/ReverseLinkedList.cs
@@ -0,0 +1,28 @@
+﻿
+using MethodsLibrary.Objects;
+
+namespace MethodsLibrary.Methods
+{
+    public class ReverseLinkedList
+    {
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+
+            ListNode fake = new ListNode(0);
+
+            while (head != null)
+            {
+                ListNode temp = head.next;
+                head.next = fake.next;
+                fake.next = head;
+                head = temp;
+            }
+
+            return fake.next;
+        }
+    }
+}
