ba42e0db7c8746a712c476b4412e1a94e3b9c18f
xxxrrraaayyy <tongbaxu@gmail.com>
2017-12-19 23:59:42 -0500

    New test for Valid Palindrome

---
+++ Methods.Tests/Tests/ValidPalindromeTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Methods.Tests.Objects;
+using MethodsLibrary.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Methods.Tests.Tests
+{
+    [TestClass]
+    class ValidPalindromeTest
+    {
+        /// <summary>
+        /// List to store test data set
+        /// </summary>
+        public List<ValidPalindromeTestData> TestDataList { set; get; }
+
+        /// <summary>
+        /// Initialize test data
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            TestDataList = new List<ValidPalindromeTestData>
+            {
+                new ValidPalindromeTestData
+                {
+                    InputString = "A man, a plan, a canal: Panama",
+                    OutputBool = true
+                },
+                new ValidPalindromeTestData
+                {
+                    InputString = "race a car",
+                    OutputBool = false
+                }
+            };
+        }
+
+        /// <summary>
+        /// Test method to verify if the method works
+        /// </summary>
+        [TestMethod]
+        public void validPalindromeTest()
+        {
+            foreach (ValidPalindromeTestData testData in TestDataList)
+            {
+                System.Console.WriteLine("Input String:" + testData.InputString);
+                Assert.AreEqual(ValidPalindrome.IsPalindrome(testData.InputString),
+                                testData.OutputBool, "Failed on the case: " + testData.InputString);
+            }
+        }
+    }
+}
---
+++ MethodsLibrary/Methods/ValidPalindrome.cs
@@ -0,0 +1,35 @@
+﻿
+namespace MethodsLibrary.Methods
+{
+    public class ValidPalindrome
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            s.Trim();
+            s.ToLower();
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start < end)
+            {
+                if (!char.IsLetter(s[start]))
+                {
+                    start++;
+                }
+                if (!char.IsLetter(s[end]))
+                {
+                    end--;
+                }
+
+                if (s[start] != s[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
